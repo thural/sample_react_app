1cb3b9cfcff08460b193dd1d4ce7cd6263de27b4
Tural Musaibov <tural.musaibov@gmail.com>
2022-08-31 16:15:42 +0400

    commit before creating form-list branch

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -4,14 +4,23 @@ class MyComponent extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      count:0,
+      message:''
+    }
+    this.countUp = this.countUp.bind(this)
   }
-
+  countUp() {
+    this.setState({count:this.state.count + 1})
+  }
   render() {
-    const {title, onButtonClicked} = this.props
+    const {title, onButtonClicked} = this.props;
     return (
       <div>
         <h3>{title}</h3>
         <button onClick={onButtonClicked}>Click Me!</button>
+        <button onClick={this.countUp}>Click to count!</button>
+        <p>Count: {this.state.count}</p>
       </div>
     );
   }
