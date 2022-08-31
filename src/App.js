865e23951735b067b166b7b3d02a1c3492c453fa
Tural Musaibov <tural.musaibov@gmail.com>
2022-08-31 19:07:59 +0400

    created a basic form with text input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-import MyComponent from './MyComponent'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.onClickBtn = this.onClickBtn.bind(this)
-  }
+  constructor() {
+    super();
 
-  onClickBtn() {
-    console.log('Button has been clicked!');
-  }
+    this.state = {
+      task: { text: '' },
+      tasks: [],
+    };
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '' },
+    });
+  };
+
   render() {
+    const { task, tasks } = this.state;
+
     return (
-      <div className="App">
-      <MyComponent title="React" onButtonClicked={this.onClickBtn} />
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            type="text"
+            id="taskInput"
+            onChange={this.handleChange}
+            value={task.text}
+          />
+          <button type="submit">
+            Add Task
+          </button>
+        </form>
       </div>
     );
   }
-  
 }
 
 export default App;
