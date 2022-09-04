010dfaf00bd432e44cdbb01ff266f164eac1742b
Tural Musaibov <tural.musaibov@gmail.com>
2022-09-04 23:59:06 +0400

    add handleEdit function for task

--- src/App.js
+++ src/App.js
@@ -9,56 +9,95 @@ class App extends Component {
     super();
 
     this.state = {
+      tasks: [],
       task: {
         text: '',
-        id: uniqid()
-      },
-      tasks: [],
+        id: uniqid(),
+        readOnly: true
+      }
     };
+
   };
 
   handleChange = (e) => {
     this.setState({
       task: {
         text: e.target.value,
-        id: this.state.task.id
+        id: this.state.task.id,
+        readOnly: true
       }
-    });
+    })
   };
 
   onSubmitTask = (e) => {
     e.preventDefault();
     this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
+      tasks: this.state.tasks
+        .concat(this.state.task),
       task: {
         text: '',
         id: uniqid()
       }
     });
   };
-  
-  deleteList = (taskId) => {
+
+  deleteTask = (id) => {
     this.setState({
-      tasks: this.state.tasks.filter(task => task.id !== taskId)
+      tasks: this.state.tasks
+        .filter(task => task.id !== id)
     })
   };
-  
+
+  toggleEdit = (id) => {
+    this.setState(state => {
+      return { tasks: state.tasks.map(task => {
+        if (task.id === id) {
+          const selected = {...task};
+          selected.readOnly = !task.readOnly;
+          return selected
+        } else return task
+      })}
+    });
+
+    console.log(this.state.tasks);
+  };
+
+  handleEdit = (e, id) => {
+    this.setState(state => {
+      return { tasks: state.tasks.map(task => {
+        if (task.id === id) {
+          const selected = {...task};
+          selected.text = e.target.value;
+          return selected
+        } else return task
+      })}
+    });
+  };
+
   render() {
     const { task, tasks } = this.state;
 
     return (
       <div>
+
         <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter task</label>
           <input
             type="text"
             id="taskInput"
-            onChange={this.handleChange}
             value={task.text}
+            onChange={this.handleChange}
           />
           <button type="submit">Add Task</button>
         </form>
-        <Overview tasks={tasks} deleteList={this.deleteList}/>
+
+        <Overview
+          tasks={tasks}
+          deleteTask={this.deleteTask}
+          toggleEdit={this.toggleEdit}
+          handleEdit={this.handleEdit}
+        />
+
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,17 +2,29 @@ import React from "react";
 
 
 const Overview = (props) => {
-  const { tasks, deleteList, editList } = props;
+  const { tasks, deleteTask, toggleEdit, handleEdit } = props;
 
   return (
     <ul>
-      {tasks.map((task) => {
-        return <li key={task.id} >
-          {task.text}
-          <button onClick={() => editList(task.id)}>edit</button>
-          <button onClick={() => deleteList(task.id)}>delete</button>
+      {
+        tasks.map(task =>
+        (
+          <li key={task.id} >
+            <input type="text"
+              id="taskInput"
+              onChange={(e) => handleEdit(e, task.id)}
+              defaultValue={task.text}
+              readOnly={task.readOnly}
+            />
+            <button onClick={() => toggleEdit(task.id)}>
+              {task.readOnly ? "edit" : "submit"}
+            </button>
+            <button onClick={() => deleteTask(task.id)}>
+              delete
+            </button>
           </li>
-      })}
+        ))
+      }
     </ul>
   );
 };
